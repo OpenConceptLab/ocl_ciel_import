82791cc57f9c3e9d50af7f7de8f2ee42d8ed8901
RohitG <rohitg@thoughtworks.com>
2016-09-01 18:13:52 +0530

    Printing to stdout and logging date in a file

--- import_full_ciel_with_docker.sh
+++ import_full_ciel_with_docker.sh
@@ -5,10 +5,10 @@ TOKEN=$2
 echo "Enqueueing import in Source -- $SOURCE"
 rm -rf import_temp
 mkdir -p import_temp
-echo "*****************" > import_temp/concepts_import.out
-echo "*****************" > import_temp/mappings_import.out
 wget -O import_temp/concept_data.json $CONCEPT_DATA_URL
 wget -O import_temp/mapping_data.json $MAPPING_DATA_URL
-date > import_temp/concepts_import.out
-date > import_temp/mappings_import.out
-docker-compose run -d --name ocl_import ocl bash -c "python manage.py import_concepts_to_source --source $SOURCE --token $TOKEN --inline-indexing true import_temp/concept_data.json >> import_temp/concepts_import.out 2>&1 && python manage.py import_mappings_to_source --source $SOURCE --token $TOKEN --inline-indexing true import_temp/mapping_data.json >> import_temp/mappings_import.out 2>&1"
+docker-compose run -d --name ocl_import ocl bash -c "python manage.py import_concepts_to_source --source $SOURCE --token $TOKEN --inline-indexing true import_temp/concept_data.json && python manage.py import_mappings_to_source --source $SOURCE --token $TOKEN --inline-indexing true import_temp/mapping_data.json"
+echo "*****************" > import_temp/concepts_import.out
+echo "*****************" > import_temp/mappings_import.out
+date >> import_temp/concepts_import.out
+date >> import_temp/mappings_import.out
