15eaf6304a4cee998a5acca114931e8975ab73a0
RohitG <rohitg@thoughtworks.com>
2016-09-01 17:44:36 +0530

    Don't need full logs so keeping single line, docker daemonized

--- import_full_ciel_with_docker.sh
+++ import_full_ciel_with_docker.sh
@@ -3,10 +3,10 @@
 echo "Enqueueing import in Source -- $1"
 rm -rf import_temp
 mkdir -p import_temp
-echo "*****************" >> import_temp/concepts_import.out
-echo "*****************" >> import_temp/mappings_import.out
+echo "*****************" > import_temp/concepts_import.out
+echo "*****************" > import_temp/mappings_import.out
 wget -O import_temp/concept_data.json $CONCEPT_DATA_URL
 wget -O import_temp/mapping_data.json $MAPPING_DATA_URL
-date >> import_temp/concepts_import.out
-date >> import_temp/mappings_import.out
-time docker-compose run ocl python manage.py import_concepts_to_source --source $1 --token $2 --inline-indexing true import_temp/concept_data.json >> import_temp/concepts_import.out 2>&1 && time docker-compose run ocl python manage.py import_mappings_to_source --source $1 --token $2 --inline-indexing true import_temp/mapping_data.json >> import_temp/mappings_import.out 2>&1
+date > import_temp/concepts_import.out
+date > import_temp/mappings_import.out
+docker-compose -d run ocl 'python manage.py import_concepts_to_source --source $1 --token $2 --inline-indexing true import_temp/concept_data.json > import_temp/concepts_import.out 2>&1 && python manage.py import_mappings_to_source --source $1 --token $2 --inline-indexing true import_temp/mapping_data.json > import_temp/mappings_import.out 2>&1'
