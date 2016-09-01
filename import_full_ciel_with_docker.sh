d31a8381f99fdd2b35034e0fce676303402c614e
RohitG <rohitg@thoughtworks.com>
2016-09-01 18:10:04 +0530

    Fixing command and using a named container to avoid conflicting jobs

--- import_full_ciel_with_docker.sh
+++ import_full_ciel_with_docker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
-echo "Enqueueing import in Source -- $1"
+SOURCE=$1
+TOKEN=$2
+echo "Enqueueing import in Source -- $SOURCE"
 rm -rf import_temp
 mkdir -p import_temp
 echo "*****************" > import_temp/concepts_import.out
@@ -9,4 +11,4 @@ wget -O import_temp/concept_data.json $CONCEPT_DATA_URL
 wget -O import_temp/mapping_data.json $MAPPING_DATA_URL
 date > import_temp/concepts_import.out
 date > import_temp/mappings_import.out
-docker-compose run -d ocl bash -c 'python manage.py import_concepts_to_source --source $1 --token $2 --inline-indexing true import_temp/concept_data.json > import_temp/concepts_import.out 2>&1 && python manage.py import_mappings_to_source --source $1 --token $2 --inline-indexing true import_temp/mapping_data.json > import_temp/mappings_import.out 2>&1'
+docker-compose run -d --name ocl_import ocl bash -c "python manage.py import_concepts_to_source --source $SOURCE --token $TOKEN --inline-indexing true import_temp/concept_data.json >> import_temp/concepts_import.out 2>&1 && python manage.py import_mappings_to_source --source $SOURCE --token $TOKEN --inline-indexing true import_temp/mapping_data.json >> import_temp/mappings_import.out 2>&1"
